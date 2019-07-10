c6ee591a62d857d23670679072614e03fdc289a3
Brittany Hartmire and Jill Hackney <bkhartmire+amnesiacsiblings@gmail.com>
2019-07-10 11:51:17 +0900

    finish get ElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,20 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (targetClass) => {
+  const result = [];
+
+  const recursiveFunc = (node) => {
+    const nodeClassList = node.className.split(" ");
+
+    if (nodeClassList.includes(targetClass)) {
+      result.push(node);
+    }
+    if (node.children.length > 0) {
+      for (const child of node.children) {
+        recursiveFunc(child);
+      }
+    }
+  };
+  recursiveFunc(document.body);
+  return result;
 };
 
 module.exports = { getElementsByClassName };
