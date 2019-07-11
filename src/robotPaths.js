787ac782d6d6b2631aa52f99001cbe2d8fca6410
Brittany Hartmire <bkhartmire@gmail.com>
2019-07-11 20:24:52 +0900

    new solution doesnt use forEach or push

--- src/robotPaths.js
+++ src/robotPaths.js
@@ -19,8 +19,6 @@ class Board {
 }
 
 class RobotPaths {
-  // initialize all your options
-  // you may want to change this code later on, too
   constructor(size) {
     this.board = new Board(size);
     this.grid = this.board.board;
@@ -32,53 +30,63 @@ class RobotPaths {
     let result = 0;
     const robotPath = this;
 
-    const recurse = (row, col) => {
+    function recurse(row, col) {
       let finishedPath = false;
       if (row === robotPath.grid.length - 1 && col === row) {
         result++;
         finishedPath = true;
       }
       if (!finishedPath) {
-        const possibleMoves = robotPath.getPossibleMoves(row, col);
-        possibleMoves.forEach((obj) => {
-          robotPath.board.togglePiece(obj.row, obj.col);
-          recurse(obj.row, obj.col);
-        });
+        up(row, col);
+        down(row, col);
+        left(row, col);
+        right(row, col);
       }
       robotPath.board.togglePiece(row, col);
-    };
+    }
+
+    function up(currentRow, currentCol) {
+      if (
+        currentRow > 0 &&
+        !robotPath.board.hasBeenVisited(currentRow - 1, currentCol)
+      ) {
+        robotPath.board.togglePiece(currentRow - 1, currentCol);
+        recurse(currentRow - 1, currentCol);
+      }
+    }
+
+    function down(currentRow, currentCol) {
+      if (
+        currentRow < robotPath.grid.length - 1 &&
+        !robotPath.board.hasBeenVisited(currentRow + 1, currentCol)
+      ) {
+        robotPath.board.togglePiece(currentRow + 1, currentCol);
+        recurse(currentRow + 1, currentCol);
+      }
+    }
+
+    function right(currentRow, currentCol) {
+      if (
+        currentCol < robotPath.grid.length - 1 &&
+        !robotPath.board.hasBeenVisited(currentRow, currentCol + 1)
+      ) {
+        robotPath.board.togglePiece(currentRow, currentCol + 1);
+        recurse(currentRow, currentCol + 1);
+      }
+    }
+
+    function left(currentRow, currentCol) {
+      if (
+        currentCol > 0 &&
+        !robotPath.board.hasBeenVisited(currentRow, currentCol - 1)
+      ) {
+        robotPath.board.togglePiece(currentRow, currentCol - 1);
+        recurse(currentRow, currentCol - 1);
+      }
+    }
     this.board.togglePiece(this.row, this.col);
     recurse(this.row, this.col);
     return result;
-  }
-
-  getPossibleMoves(currentRow, currentCol) {
-    const possibleMoves = [];
-    if (
-      currentCol > 0 &&
-      !this.board.hasBeenVisited(currentRow, currentCol - 1)
-    ) {
-      possibleMoves.push({ row: currentRow, col: currentCol - 1 });
-    }
-    if (
-      currentCol < this.grid.length - 1 &&
-      !this.board.hasBeenVisited(currentRow, currentCol + 1)
-    ) {
-      possibleMoves.push({ row: currentRow, col: currentCol + 1 });
-    }
-    if (
-      currentRow > 0 &&
-      !this.board.hasBeenVisited(currentRow - 1, currentCol)
-    ) {
-      possibleMoves.push({ row: currentRow - 1, col: currentCol });
-    }
-    if (
-      currentRow < this.grid.length - 1 &&
-      !this.board.hasBeenVisited(currentRow + 1, currentCol)
-    ) {
-      possibleMoves.push({ row: currentRow + 1, col: currentCol });
-    }
-    return possibleMoves;
   }
 }
 
